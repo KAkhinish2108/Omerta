efe1ecfc9d0bf9e17d00f0a42a9c9636fcc1cccb
KAkhinish <akhinish.acad1009@gmail.com>
2025-11-21 10:25:01 +0530

    Added JS file for light to dark theme

---
+++ script.js
@@ -0,0 +1,8 @@
+  const toggle = document.querySelector(".theme-toggle");
+  toggle.addEventListener("click", () => {
+    document.documentElement.classList.toggle("light");
+    toggle.textContent = 
+      document.documentElement.classList.contains("light")
+      ? "Switch to Dark"
+      : "Switch to Light";
+  });
