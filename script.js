a4c2140ab509f8c569f8e78e20cb3775a1a78d6a
KAkhinish <akhinish.acad1009@gmail.com>
2025-12-05 02:16:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-  const toggle = document.querySelector(".theme-toggle");
-  toggle.addEventListener("click", () => {
-    document.documentElement.classList.toggle("light");
-    toggle.textContent = 
-      document.documentElement.classList.contains("light")
-      ? "🌙"
-      : "☀️";
-  });
